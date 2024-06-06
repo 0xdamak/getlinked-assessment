99c769869dd950877b8407e4d68335d7c7bca7ef
Damilola Akinlade <damak.dev@gmail.com>
2024-06-06 09:06:31 +0100

    colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      "gl-black": "#000000",
+      "gl-white": "#FFFFFF",
+      "gl-gray": {
+        100: "#F8F9FB",
+        500: "#8C8CA1",
+        800: "#4A4A68",
+        900: "#0E0E2C",
+      },
+      "gl-purple": {
+        50: "#F5F3FF",
+        100: "#ECE8FF",
+      },
+      "gl-indigo": {
+        600: "#755AE2",
+        700: "#675E8B",
+      },
+      "gl-orange": {
+        500: "#FF5F56",
       },
     },
+    extend: {},
   },
   plugins: [],
 };
